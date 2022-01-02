6298b875b6153d0aba96ab86750dc399fb5a269c
mierzvoj <mierzvoj@gmail.com>
2022-01-02 19:21:20 +0100

    project hosted on heroku

--- server.js
+++ server.js
@@ -5,4 +5,4 @@ app.use(express.static(__dirname + '/dist/mojeprzejazdy'));
 app.get('/login', function(req,res) {
 res.sendFile(path.join(__dirname+
 '/dist/mojeprzejazdy/index.html'));});
-app.listen(process.env.PORT || 8000);
+app.listen(process.env.PORT || 8080);
