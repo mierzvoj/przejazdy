01cfb814b0f87c7f1071a660a55d1ad59594cd38
mierzvoj <mierzvoj@gmail.com>
2022-01-02 14:29:17 +0100

    some elemenent are taking its final look, some heroku deployment modifications were necessary

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = express();
 app.use(express.static(__dirname + '/dist/przejazdy'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/przejazdy/index.html'));});
+'/dist/przejazdy/#/login'));});
 app.listen(process.env.PORT || 8080);
