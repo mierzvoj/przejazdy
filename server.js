edf2453c12d17689273ae488ca44aec171f1b3d6
mierzvoj <mierzvoj@gmail.com>
2022-01-03 21:39:37 +0100

    project hosted on heroku, CORS issue still unresolved, fighting errors

--- server.js
+++ server.js
@@ -1,29 +1,10 @@
 const express = require('express');
 const path = require('path');
-const cors = require('cors');
+
 
 const app = express();
 
-// add this code
-const whitelist = ['https://awps-dev.herokuapp.com/']; // list of allow domain
 
-const corsOptions = {
-    origin: function (origin, callback) {
-        if (!origin) {
-            return callback(null, true);
-        }
-
-        if (whitelist.indexOf(origin) === -1) {
-            var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    }
-}
-
-// end 
-app.use(cors(corsOptions));
 
 app.use(express.static(__dirname + '/dist/mojeprzejazdy'));
 app.get('/login', function(req,res) {
